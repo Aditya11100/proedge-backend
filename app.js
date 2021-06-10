c40511936e11aab1a448ac47d47a32bddbe1b6c4
Aditya Raj <63391244+Aditya11100@users.noreply.github.com>
2021-06-10 16:43:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let result = {};
 app.post("/getroll", async (req, res) => {
     let roll = req.body.roll;
     getroll = roll.split(",");
-    await Promise(
+    await Promise.all(
         getroll.map((e) =>
             axios
                 .get(`http://proedge.me/test.php?rollnumber=${parseInt(e)}`)
